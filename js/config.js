907bac49f1b053a41507c41232deadc0d4146895
sauce-coder-2025 <luke.gray@fisherpaykel.com>
2024-12-12 11:49:09 +1300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -61,6 +61,105 @@ const CONFIG = {
                 'Home Solutions': 'HSAU',
                 'DCS': 'DCSAU'
             },
+            'NZ': {
+                'F&P': 'FPNZ',
+                'Haier': 'HAINZ',
+                'Haier Home': 'HHNZ',
+                'Home Solutions': 'HSNZ'
+            },
+            'US': {
+                'F&P': 'FPUS',
+                'DCS': 'DCSUS'
+            },
+            'UK': {
+                'F&P': 'FPUK'
+            },
+            'CA': {
+                'F&P': 'FPCA',
+                'DCS': 'DCSCA'
+            },
+            'SG': {
+                'F&P': 'FPSG'
+            },
+            'GBL': {
+                'F&P': 'FPGBL',
+                'Haier': 'HAIGBL',
+                'Haier Home': 'HHGBL',
+                'Home Solutions': 'HSGBL',
+                'DCS': 'DCSGBL'
+            }
+        },
+        mediaObjective: {
+            'Attract': 'ATT',
+            'Engage': 'ENG',
+            'Convert': 'CON',
+            'Retain': 'RET'
+        },
+        month: {
+            'January': 'JAN',
+            'February': 'FEB',
+            'March': 'MAR',
+            'April': 'APR',
+            'May': 'MAY',
+            'June': 'JUN',
+            'July': 'JUL',
+            'August': 'AUG',
+            'September': 'SEP',
+            'October': 'OCT',
+            'November': 'NOV',
+            'December': 'DEC'
+        },
+        buyType: {
+            'QR Code': 'QR',
+            'Email': 'EMAIL',
+            'Website': 'WEB',
+            'Social': 'SOC',
+            'Paid Social': 'PSOC',
+            'Offline': 'OFF',
+            'Reach': 'REACH',
+            'Traffic': 'TRAFFIC',
+            'Conversion': 'CONV',
+            'Engagement': 'ENG',
+            'Video Views': 'VIDV'
+        },
+        category: {
+            'Cooling': 'COOL',
+            'Cooking': 'COOK',
+            'Fabrice Care': 'FAB',
+            'Dishwashing': 'DISH',
+            'Outdoor': 'OUT',
+            'Ventilation': 'VENT',
+            'Accessories': 'ACC',
+            'Promotions': 'PROMO'
+        },
+        subCategory: {
+            'Refrigeration': 'REF',
+            'Wine': 'WINE',
+            'Chest Freezer': 'FREEZE',
+            'Ovens': 'OVEN',
+            'Cooktops': 'CTOP',
+            'Freestanding': 'FREE',
+            'Companions': 'COMP',
+            'Washing Machines': 'WASH',
+            'Dryers': 'DRY',
+            'Cabinets': 'CAB',
+            'Grills': 'GRILL',
+            'Carts': 'CART',
+            'Storage': 'STOR',
+            'Spare Parts': 'SPARE',
+            'Accessories': 'ACC',
+            'Water Filters': 'FILT',
+            'Cleaning': 'CLEAN'
+        }
+    },
+        markets: {
+            'AU': {
+                'F&P': 'FPAU',
+                'Haier': 'HAIAU',
+                'Haier Home': 'HHAU',
+                'Home Solutions': 'HSAU',
+                'DCS': 'DCSAU'
+            },
             // Add other market abbreviations...
         },
         mediaObjective: {
