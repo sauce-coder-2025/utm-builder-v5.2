e1c32762191f96fdbe7943783dc44d7eb65d2ae2
sauce-coder-2025 <luke.gray@fisherpaykel.com>
2024-12-16 08:40:41 +1300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,12 @@
+const firebaseConfig = {
+      apiKey: "AIzaSyAeA92bKST_oMfhKqqKuaUh7eN5Qdqg1BQ",
+      authDomain: "fisherpaykel-utm-builder-app.firebaseapp.com",
+      projectId: "fisherpaykel-utm-builder-app",
+      storageBucket: "fisherpaykel-utm-builder-app.firebasestorage.app",
+      messagingSenderId: "98977816783",
+      appId: "1:989778167843:web:96555b3ed1149a7a6dc996",
+      measurementId: "G-DZ6MCDJVDY"
+    };
 const CONFIG = {
     // Market-Brand dependencies
     marketBrands: {
